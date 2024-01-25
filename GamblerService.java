595c8d3f79cccc352800cd74d2d1bb1375dd6e23
Stavin Alban <stavinalban@gmail.com>
2024-01-25 11:54:45 +0530

    updated code (resolved error(error in passing limit))

--- GamblerService.java
+++ GamblerService.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class GamblerService {
     public void limits(Gambler g){
         Scanner sc=new Scanner(System.in);
+
+        // Setting Higher limit
         System.out.println("Enter the highest limit of the stack that has to be reached");
         int hl= sc.nextInt();
-        g.setStack(hl);
+        g.setHightLimit(hl);
+
+        //Setting Lower limit
         System.out.println("Enter the Lower limit of the stack that has to be reached");
         int ll= sc.nextInt();
-        g.setStack(ll);
+        g.setLowLimit(ll);
     }
 
     public void game(Gambler g){
